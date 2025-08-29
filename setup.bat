bc051a3fb698d578e9651ccad0a0a37222b91d17
anshulpatel3535 <anshulpatel3535@gmail.com>
2025-08-29 22:03:58 +0000

    Fix: Standardize python command to python3 in setup.bat

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ REM This script automates the setup of the local development environment on Wind
 
 ECHO --- Setting up Python virtual environment --- 
 REM Create a virtual environment named 'venv' if it doesn't exist
-IF NOT EXIST venv ( python -m venv venv )
+IF NOT EXIST venv ( python3 -m venv venv )
 
 REM Activate the virtual environment
 call .\venv\Scripts\activate
@@ -14,10 +14,10 @@ pip install -r requirements.txt
 ECHO.\n--- Running the ETL script ---
 REM The Python script will now handle database creation automatically.
 REM It will also provide instructions if the database user needs to be created.
-python etl.py
+python3 etl.py
 
 ECHO.\n--- Setup Complete ---
 ECHO The virtual environment is active, libraries are installed, and the ETL script has been run.
 ECHO To run the script again in the future, just activate the environment and run the python script:
 ECHO   .\venv\Scripts\activate
-ECHO   python etl.py
+ECHO   python3 etl.py
